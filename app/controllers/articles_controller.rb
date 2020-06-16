7de302e2030a3859a12cdf4697077312bb9264f7
afzalMa <afzal.m@etvbharat.com>
2020-06-16 16:08:44 +0530

    added the new create article from desktop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,18 @@ class ArticlesController < ApplicationController
     def show
        @article = Article.find(params[:id]) 
     end
+
     def index
         @article = Article.all
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article
+    end
+
+    def new
+        
     end
 end
